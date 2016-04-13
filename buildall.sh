a79a5486fe950d19f6d3eb3b66ab209158ad417e
Tim Armstrong <tarmstrong@cloudera.com>
2016-04-13 13:51:46 -0700

    Add snappy 1.1.3
    Change-Id: I75d393f79f9fcad7fb7127e63c538a6799c950e6

--- buildall.sh
+++ buildall.sh
@@ -153,6 +153,7 @@ GOOGLETEST_VERSION=20151222 $SOURCE_DIR/source/googletest/build.sh
 # Build Snappy
 ################################################################################
 SNAPPY_VERSION=1.0.5 $SOURCE_DIR/source/snappy/build.sh
+SNAPPY_VERSION=1.1.3 $SOURCE_DIR/source/snappy/build.sh
 
 ################################################################################
 # Build Lz4
