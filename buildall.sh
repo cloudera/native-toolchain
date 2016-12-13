41bb87384b849b9131d597544b022a65cae43cb3
Henry Robinson <henry@cloudera.com>
2016-12-12 22:45:59 -0800

    IMPALA-4651: Add libev 4.20 to toolchain
    Change-Id: I4c179741253d009984e2715ab2f2f9a5c47288ec

--- buildall.sh
+++ buildall.sh
@@ -92,6 +92,11 @@ LIBEVENT_VERSION=1.4.15 $SOURCE_DIR/source/libevent/build.sh
 PROTOBUF_VERSION=2.6.1 $SOURCE_DIR/source/protobuf/build.sh
 
 ################################################################################
+# Build libev
+################################################################################
+LIBEV_VERSION=4.20 $SOURCE_DIR/source/libev/build.sh
+
+################################################################################
 # Build OpenSSL - this is not intended for production use of Impala.
 # Libraries that depend on OpenSSL will only use it if PRODUCTION=1.
 ################################################################################
