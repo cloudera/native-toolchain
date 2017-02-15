715ee51bdcc66890cf6eefd79b719cc881830a0a
Henry Robinson <henry@cloudera.com>
2017-02-15 11:21:39 -0800

    Force LZ4 to build static libs
    Change-Id: I8960a84343c3c0b75090dbae252e642ef932cba4

--- source/lz4/build.sh
+++ source/lz4/build.sh
@@ -32,7 +32,7 @@ if needs_build_package ; then
       CFLAGS=-fPIC
       cd contrib/cmake_unofficial
   fi
-  wrap cmake -DCMAKE_INSTALL_PREFIX=$LOCAL_INSTALL .
+  wrap cmake -DBUILD_STATIC_LIBS=ON -DCMAKE_INSTALL_PREFIX=$LOCAL_INSTALL .
   wrap make -j${BUILD_THREADS:-4} install
   footer $PACKAGE $PACKAGE_VERSION
 fi
