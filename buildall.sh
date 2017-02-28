34b2c8552d7d1d3ff701b205cab40e74f50b07f5
Lars Volker <lv@cloudera.com>
2017-02-28 14:23:02 -0800

    Bump breakpad version
    This will give us better support for DWARF version 4,
    support for redaction of sensitive data, and a multitude of bug fixes.
    
    Our previous version has been from June 12th 2015, this change bumps it
    to Feb 28th 2017.
    
    It also includes a script from Kudu that can be used to create tarballs
    from the Breakpad Git repository. Breakpad itself does not do releases.
    
    Change-Id: I8859ced5048a9f3098c9ec3f24e73311111700a1

--- buildall.sh
+++ buildall.sh
@@ -247,7 +247,7 @@ LIBUNWIND_VERSION=1.1 $SOURCE_DIR/source/libunwind/build.sh
 ################################################################################
 # Build Breakpad
 ################################################################################
-BREAKPAD_VERSION=20150612-p1 $SOURCE_DIR/source/breakpad/build.sh
+BREAKPAD_VERSION=88e5b2c8806bac3f2c80d2fe80094be5bd371601 $SOURCE_DIR/source/breakpad/build.sh
 
 ################################################################################
 # Build Flatbuffers
