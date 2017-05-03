5fbf43fa0fac8e8e41e6e27f4aa3ee5596550731
Matthew Jacobs <mj@cloudera.com>
2017-05-03 15:30:55 -0700

    Bump Kudu version to 7533364.
    Change-Id: Ia769446fbcd265977859d3e20a6acfff1d89344b

--- buildall.sh
+++ buildall.sh
@@ -267,7 +267,7 @@ FLATBUFFERS_VERSION=1.6.0 $SOURCE_DIR/source/flatbuffers/build.sh
 ################################################################################
 (
   export BOOST_VERSION=1.57.0-p1
-  export KUDU_VERSION=238249c
+  export KUDU_VERSION=7533364
   if $SOURCE_DIR/source/kudu/build.sh is_supported_platform; then
     $SOURCE_DIR/source/kudu/build.sh build
   else
