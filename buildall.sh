acd370564118fa9b4ca9fdd89c66a69e77a3f6ba
Thomas Tauber-Marshall <tmarshall@cloudera.com>
2017-05-22 14:58:37 -0700

    Bumped Kudu version to 795c435.
    This is needed for IMPALA-5167.
    
    Change-Id: If16f905ca719c257456fe8381ef503cf5edd5728

--- buildall.sh
+++ buildall.sh
@@ -267,7 +267,7 @@ FLATBUFFERS_VERSION=1.6.0 $SOURCE_DIR/source/flatbuffers/build.sh
 ################################################################################
 (
   export BOOST_VERSION=1.57.0-p1
-  export KUDU_VERSION=7533364
+  export KUDU_VERSION=795c435
   if $SOURCE_DIR/source/kudu/build.sh is_supported_platform; then
     $SOURCE_DIR/source/kudu/build.sh build
   else
