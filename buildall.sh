9a2affdeab3d44fdcc494f96e56ff0342446542f
Thomas Tauber-Marshall <tmarshall@cloudera.com>
2017-09-11 08:17:53 -0700

    Bump Kudu version to 3f49724
    Change-Id: I38334bdbaa1e7a6c6f5d595c26efb76efa062305

--- buildall.sh
+++ buildall.sh
@@ -284,7 +284,7 @@ FLATBUFFERS_VERSION=1.6.0 $SOURCE_DIR/source/flatbuffers/build.sh
 ################################################################################
 (
   export BOOST_VERSION=1.57.0-p3
-  export KUDU_VERSION=a71ecfd
+  export KUDU_VERSION=3f49724
   if $SOURCE_DIR/source/kudu/build.sh is_supported_platform; then
     $SOURCE_DIR/source/kudu/build.sh build
   else
