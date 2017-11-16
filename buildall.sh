abd41bcac4774fddbf9df303304cf15023fdd7ea
Laszlo Gaal <laszlo.gaal@cloudera.com>
2017-11-16 17:46:50 +0100

    Bump Kudu version to cdh5-1.4.0_5.12.2
    Prepare for CDH5.12.2
    
    Change-Id: Ide7bf8add114f05853388ca72f493cd9fce70e34

--- buildall.sh
+++ buildall.sh
@@ -268,7 +268,7 @@ FLATBUFFERS_VERSION=1.6.0 $SOURCE_DIR/source/flatbuffers/build.sh
 ################################################################################
 (
   export BOOST_VERSION=1.57.0-p2
-  export KUDU_VERSION=kudu5.12.1-rc1
+  export KUDU_VERSION=cdh5-1.4.0_5.12.2
   export KUDU_GITHUB_URL=http://github.mtv.cloudera.com/CDH/kudu
   if $SOURCE_DIR/source/kudu/build.sh is_supported_platform; then
     $SOURCE_DIR/source/kudu/build.sh build
