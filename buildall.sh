f041cff94e82695c8b652fe5fa3dc530ff6b6442
Lars Volker <lv@cloudera.com>
2018-01-10 04:00:10 -0800

    IMPALA-6377: Bump breakpad version to include fix for #752
    This change bumps the Breakpad version in the toolchain to pull in the
    fix for https://bugs.chromium.org/p/google-breakpad/issues/detail?id=752 .
    
    It also copies the DWZ patch required for IMPALA-4227 to be applied to
    the new version.
    
    This change also removes the PPC patches from Breakpad, which I was not
    able to get to apply cleanly. They will be re-applied in a later change.
    
    Change-Id: I9b6212ab77eff2df0cb51339c25183ae4f22b07b

--- buildall.sh
+++ buildall.sh
@@ -266,7 +266,7 @@ LIBUNWIND_VERSION=1.1-p1 $SOURCE_DIR/source/libunwind/build.sh
 if (( BUILD_HISTORICAL )); then
   BREAKPAD_VERSION=20150612-p1 $SOURCE_DIR/source/breakpad/build.sh
 fi
-BREAKPAD_VERSION=1b704857f1e78a864e6942e613457e55f1aecb60-p3 $SOURCE_DIR/source/breakpad/build.sh
+BREAKPAD_VERSION=97a98836768f8f0154f8f86e5e14c2bb7e74132e-p1 $SOURCE_DIR/source/breakpad/build.sh
 
 ################################################################################
 # Build Flatbuffers
