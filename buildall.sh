d95bb7f778c95ad1053dd64dd083f4da33ddb11e
Thomas Tauber-Marshall <tmarshall@cloudera.com>
2018-02-15 10:21:42 -0800

    Bump Kudu version to 0eef8e0
    This pulls in support for DECIMAL.
    
    Change-Id: I186317c3440e1a50fffa87e67b23166da7715e1a

--- buildall.sh
+++ buildall.sh
@@ -290,7 +290,7 @@ FLATBUFFERS_VERSION=1.6.0 $SOURCE_DIR/source/flatbuffers/build.sh
 ################################################################################
 (
   export BOOST_VERSION=1.57.0-p3
-  export KUDU_VERSION=b315d0e
+  export KUDU_VERSION=0eef8e0
   if $SOURCE_DIR/source/kudu/build.sh is_supported_platform; then
     $SOURCE_DIR/source/kudu/build.sh build
   else
