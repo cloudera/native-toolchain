453692f68b97cdf5ee1dcabd3af0729c1ba541c2
Sahil Takiar <takiar.sahil@gmail.com>
2018-11-29 15:20:36 -0500

    IMPALA-7924: Add Thrift 0.11.0 to the toolchain
    Adds Thrift 0.11.0 to the toolchain. One patch from 0.9.3 is carried
    over that preserves compatibility forward compatibility of TLS
    protocols. Another path was added (THRIFT-4417), which is required for
    fb303.thrift C++ code to build.
    
    Change-Id: Idb07f43e70844b11ec79c708cab3820628a44cd2
    Reviewed-on: http://gerrit.cloudera.org:8080/12034
    Reviewed-by: Lars Volker <lv@cloudera.com>
    Tested-by: Lars Volker <lv@cloudera.com>

--- buildall.sh
+++ buildall.sh
@@ -158,6 +158,7 @@ if [[ ! "$OSTYPE" == "darwin"* ]]; then
   # thrift-0.9.3.
   THRIFT_VERSION=0.9.0-p11 $SOURCE_DIR/source/thrift/build.sh
   THRIFT_VERSION=0.9.3-p4 $SOURCE_DIR/source/thrift/build.sh
+  THRIFT_VERSION=0.11.0-p2 $SOURCE_DIR/source/thrift/build.sh
 else
   THRIFT_VERSION=0.9.2-p2 $SOURCE_DIR/source/thrift/build.sh
 fi
