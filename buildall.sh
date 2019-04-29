53d8a71d77c6329e24a87d223a22817a3549f896
Thomas Tauber-Marshall <tmarshall@cloudera.com>
2019-04-29 13:42:10 -0700

    Bump Kudu version to 9ba901a
    This pulls in the new API for column comments needed for IMPALA-5351.
    
    Change-Id: I046410008a362f5e84210c36bf15a3e322305ccc
    Reviewed-on: http://gerrit.cloudera.org:8080/13194
    Reviewed-by: Fredy Wijaya <fwijaya@cloudera.com>
    Tested-by: Thomas Marshall <tmarshall@cloudera.com>

--- buildall.sh
+++ buildall.sh
@@ -306,7 +306,7 @@ FLATBUFFERS_VERSION=1.6.0 $SOURCE_DIR/source/flatbuffers/build.sh
 ################################################################################
 (
   export BOOST_VERSION=1.57.0-p3
-  export KUDU_VERSION=1.9.0
+  export KUDU_VERSION=9ba901a
   if $SOURCE_DIR/source/kudu/build.sh is_supported_platform; then
     $SOURCE_DIR/source/kudu/build.sh build
   else
