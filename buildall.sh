96a6827be2b054f58655a09df6f0799026048a83
huangtianhua <huangtianhua223@gmail.com>
2020-01-23 03:17:01 +0000

    Add missing boost patches back for 1.61.0
    Change-Id: Ic5fc317c20ec63456c4e411c61415da3b0581da7
    Reviewed-on: http://gerrit.cloudera.org:8080/15095
    Reviewed-by: Tim Armstrong <tarmstrong@cloudera.com>
    Tested-by: Tim Armstrong <tarmstrong@cloudera.com>

--- buildall.sh
+++ buildall.sh
@@ -51,7 +51,7 @@ if [[ "$ARCH_NAME" != "aarch64" ]]; then
   fi
   BOOST_VERSION=1.57.0-p3 $SOURCE_DIR/source/boost/build.sh
 fi
-BOOST_VERSION=1.61.0 $SOURCE_DIR/source/boost/build.sh
+BOOST_VERSION=1.61.0-p2 $SOURCE_DIR/source/boost/build.sh
 
 ################################################################################
 # Build Python
@@ -149,7 +149,7 @@ BISON_VERSION=3.0.4 $SOURCE_DIR/source/bison/build.sh
 #  * depends on bison, boost, zlib and openssl
 ################################################################################
 export BISON_VERSION=3.0.4
-export BOOST_VERSION=1.57.0-p3
+export BOOST_VERSION=1.61.0-p2
 export ZLIB_VERSION=1.2.8
 export OPENSSL_VERSION=1.0.2l
 
