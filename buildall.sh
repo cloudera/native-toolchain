3f59defd79841ca9ebd0707f3bffa41817552915
zhaorenhai <zhaorenhai@hotmail.com>
2020-04-03 08:54:13 +0000

    IMPALA-10088 Fix DeadLock while run unifiedbetests on aarch64 platform
    Add getting stacktrace implementation on aarch64 without using libunwind and gcc,
    to solve this issue:  https://github.com/gperftools/gperftools/issues/1184 .
    This is a reference implementation of :
    https://github.com/abseil/abseil-cpp/blob/master/absl/debugging/internal/stacktrace_aarch64-inl.inc
    
    Change-Id: I8d7fffde111d0a57826291cec90df76194d58530
    Reviewed-on: http://gerrit.cloudera.org:8080/16357
    Reviewed-by: Tim Armstrong <tarmstrong@cloudera.com>
    Tested-by: Tim Armstrong <tarmstrong@cloudera.com>

--- buildall.sh
+++ buildall.sh
@@ -210,7 +210,7 @@ if (( BUILD_HISTORICAL )); then
   GPERFTOOLS_VERSION=2.3 $SOURCE_DIR/source/gperftools/build.sh
 fi
 # IMPALA-6414: Required until issues with 2.6.3 have been sorted out.
-GPERFTOOLS_VERSION=2.5-p1 $SOURCE_DIR/source/gperftools/build.sh
+GPERFTOOLS_VERSION=2.5-p2 $SOURCE_DIR/source/gperftools/build.sh
 GPERFTOOLS_VERSION=2.6.3 $SOURCE_DIR/source/gperftools/build.sh
 
 ################################################################################
