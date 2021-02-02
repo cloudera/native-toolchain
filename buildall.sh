4ad03513e3b4b4e2e7a2e942ff19ead2be15326c
Alexey Serbin <alexey@apache.org>
2021-02-01 17:05:13 -0800

    [config] bump Kudu version to 1.14
    The motivation for this version patch is two-fold:
    
      * Update the version of Kudu client to reflect the recently
        released Kudu 1.14 (see https://kudu.apache.org/releases/1.14.0/)
    
      * Pick up https://gerrit.cloudera.org/#/c/16705 change to control
        Kudu client connection negotiation timeout in impalad
    
    Change-Id: I5e75ca996670a7abf161f0c5e7751031391fd959
    Reviewed-on: http://gerrit.cloudera.org:8080/17016
    Reviewed-by: Thomas Tauber-Marshall <tmarshall@cloudera.com>
    Tested-by: Thomas Tauber-Marshall <tmarshall@cloudera.com>

--- buildall.sh
+++ buildall.sh
@@ -319,7 +319,7 @@ FLATBUFFERS_VERSION=1.6.0 $SOURCE_DIR/source/flatbuffers/build.sh
 ################################################################################
 (
   export BOOST_VERSION=1.61.0-p2
-  export KUDU_VERSION=6a7cadc7e
+  export KUDU_VERSION=b5e7362e69
   export PYTHON_VERSION=2.7.16
   if $SOURCE_DIR/source/kudu/build.sh is_supported_platform; then
     $SOURCE_DIR/source/kudu/build.sh build
