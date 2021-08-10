9a439f6443a1bbfba8d32415e0e6d98e2e7aaed7
stiga-huang <huangquanlong@gmail.com>
2021-08-10 16:03:37 +0800

    IMPALA-10796: Add Arrow 4.0.1 to native-toolchain
    Currently only the c++ JSON reader module is added. We can add more in
    the future in need.
    
    Change-Id: I2adb1807ff4ca38596461ea6e5dd2ce758bcd1bf
    Reviewed-on: http://gerrit.cloudera.org:8080/17766
    Reviewed-by: Wenzhe Zhou <wzhou@cloudera.com>
    Reviewed-by: Aman Sinha <amsinha@cloudera.com>
    Tested-by: Quanlong Huang <huangquanlong@gmail.com>

---
+++ source/arrow/build.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+# Copyright 2018 Cloudera Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Exit on non-true return value
+set -e
+# Exit on reference to uninitialized variable
+set -u
+
+source $SOURCE_DIR/functions.sh
+THIS_DIR="$( cd "$( dirname "$0" )" && pwd )"
+prepare $THIS_DIR
+
+if needs_build_package ; then
+  download_dependency $PACKAGE "${PACKAGE_STRING}.tar.gz" $THIS_DIR
+  setup_package_build $PACKAGE $PACKAGE_VERSION
+
+  cd cpp
+  wrap cmake -DARROW_JSON=ON -DARROW_OPTIONAL_INSTALL=ON -DARROW_JEMALLOC=OFF \
+      -DCMAKE_INSTALL_PREFIX=$LOCAL_INSTALL -DCMAKE_BUILD_TYPE=RELEASE
+  CFLAGS="-fPIC -DPIC" wrap make VERBOSE=1 -j${BUILD_THREADS:-4} all install
+  finalize_package_build $PACKAGE $PACKAGE_VERSION
+fi
