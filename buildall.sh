4fde3e799dac2ded51ae30788f40bed331fa7a95
stiga-huang <huangquanlong@gmail.com>
2021-11-30 16:10:44 +0800

    IMPALA-11037: Bump ORC to 1.7.0-p4 to contain ORC-1020
    ORC-1020 improves read performance of the ORC library in scanning random
    integers. Columns that encoded into integers, e.g. dictionary encoded
    strings, will also benifit from this. This patch bumps the ORC version
    from 1.7.0-p3 to 1.7.0-p4 to have the improvement.
    
    Test:
     - Build ORC locally.
    
    Change-Id: Id0d3cc3b357e2f03f2eca7c289ec376d12d58061
    Reviewed-on: http://gerrit.cloudera.org:8080/18058
    Reviewed-by: Quanlong Huang <huangquanlong@gmail.com>
    Tested-by: Quanlong Huang <huangquanlong@gmail.com>

--- buildall.sh
+++ buildall.sh
@@ -367,11 +367,10 @@ KRB5_VERSION=1.15.1 $SOURCE_DIR/source/krb5/build.sh
   if (( BUILD_HISTORICAL )); then
     ORC_VERSION=1.4.3-p3 $SOURCE_DIR/source/orc/build.sh
     ORC_VERSION=1.5.5-p1 $SOURCE_DIR/source/orc/build.sh
-    ORC_VERSION=1.6.2-p6 $SOURCE_DIR/source/orc/build.sh
-    ORC_VERSION=1.6.2-p7 $SOURCE_DIR/source/orc/build.sh
     ORC_VERSION=1.6.2-p11 $SOURCE_DIR/source/orc/build.sh
+    ORC_VERSION=1.7.0-p3 $SOURCE_DIR/source/orc/build.sh
   fi
-  ORC_VERSION=1.7.0-p3 $SOURCE_DIR/source/orc/build.sh
+  ORC_VERSION=1.7.0-p4 $SOURCE_DIR/source/orc/build.sh
 )
 
 ################################################################################
