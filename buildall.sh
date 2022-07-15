f7f6fd62def93b998012c871e7554e39b3c2f399
Joe McDonnell <joemcdonnell@cloudera.com>
2022-07-15 08:17:51 -0700

    IMPALA-11430: Bump Kudu version to 43ee785b2d
    This bumps KUDU_VERSION to 43ee785b2d to pick up Kudu
    changes needed for the custom hash schema for
    range partitioning.
    
    Change-Id: I7b3455ab56037bdc082f70298af914eb7c70ebbd
    Reviewed-on: http://gerrit.cloudera.org:8080/18737
    Reviewed-by: Wenzhe Zhou <wzhou@cloudera.com>
    Tested-by: Joe McDonnell <joemcdonnell@cloudera.com>

--- buildall.sh
+++ buildall.sh
@@ -328,7 +328,7 @@ FLATBUFFERS_VERSION=1.6.0 $SOURCE_DIR/source/flatbuffers/build.sh
 ################################################################################
 (
   export BOOST_VERSION=1.74.0-p1
-  export KUDU_VERSION=09a6c8833e
+  export KUDU_VERSION=43ee785b2d
   export PYTHON_VERSION=2.7.16
   if $SOURCE_DIR/source/kudu/build.sh is_supported_platform; then
     $SOURCE_DIR/source/kudu/build.sh build
