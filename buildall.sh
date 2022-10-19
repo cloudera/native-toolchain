5370257ea70d96f337e80ba973ba8acb5270f95d
stiga-huang <huangquanlong@gmail.com>
2022-10-19 13:35:36 +0800

    IMPALA-11709: Add Arrow 9.0.0-p2 to native-toolchain
    This patch adds Arrow 9.0.0 release along with the two following fixes
    to native-toolchain:
    ARROW-17847: [C++] Support unquoted decimal in JSON parser
    ARROW-17995: [C++] Fix json decimals not being rescaled based on the
    explicit schema
    
    Note that the latest Arrow release is 10.0.0 which contains the two
    fixes. However, Arrow 10.0.0 mandates C++ 17 (ARROW-17545), which leads
    to compile errors with Impala (e.g. std::string_view undefined). So the
    highest version we can try is 9.0.0.
    
    Tests:
     - Built locally
     - Verified the build with Impala
    
    Change-Id: I6b47bb1370fe8a9b1a6d48d54fe629ebed395cde
    Reviewed-on: http://gerrit.cloudera.org:8080/19219
    Reviewed-by: Michael Smith <michael.smith@cloudera.com>
    Tested-by: Quanlong Huang <huangquanlong@gmail.com>

--- buildall.sh
+++ buildall.sh
@@ -390,7 +390,10 @@ JWT_CPP_VERSION=0.5.0 $SOURCE_DIR/source/jwt-cpp/build.sh
 ################################################################################
 # ARROW
 ################################################################################
-ARROW_VERSION=4.0.1 $SOURCE_DIR/source/arrow/build.sh
+if (( BUILD_HISTORICAL )); then
+  ARROW_VERSION=4.0.1 $SOURCE_DIR/source/arrow/build.sh
+fi
+ARROW_VERSION=9.0.0-p2 $SOURCE_DIR/source/arrow/build.sh
 
 # CURL
 ################################################################################
