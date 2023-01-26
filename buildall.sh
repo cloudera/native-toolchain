c284a9372ea654b45d5c05a9f15e201be278173e
wzhou-code <wzhou@cloudera.com>
2023-01-26 11:03:41 -0800

    IMPALA-11809: Bump Kudu version to 345fd44ca3
    This bumps KUDU_VERSION to the latest upstream Kudu 345fd44ca3 to pick
    up Kudu changes needed for supporting non-unique primary key.
    
    The issue seen in IMPALA-11441 was caused by KUDU-1644. It has been
    fixed with KUDU-3384 so we can switch back to upstream Kudu repo.
    
    Testing:
     - Passed Impala exhaustive test.
    
    Change-Id: Ib0b668982c9d129f5bcb8abd0b4975bbb0fce29e
    Reviewed-on: http://gerrit.cloudera.org:8080/19454
    Reviewed-by: Joe McDonnell <joemcdonnell@cloudera.com>
    Tested-by: Wenzhe Zhou <wzhou@cloudera.com>

--- buildall.sh
+++ buildall.sh
@@ -330,11 +330,7 @@ FLATBUFFERS_VERSION=1.9.0-p1 $SOURCE_DIR/source/flatbuffers/build.sh
 ################################################################################
 (
   export BOOST_VERSION=1.74.0-p1
-  # IMPALA-11441: KUDU-1644 caused a regression in an Impala upsert test,
-  # so this is the equivalent of upstream Kudu dc4031f693 with KUDU-1644
-  # reverted.
-  export KUDU_VERSION=956093dd9d
-  export KUDU_GITHUB_URL=https://github.com/joemcdonnell/kudu.git
+  export KUDU_VERSION=345fd44ca3
   export PYTHON_VERSION=2.7.16
   if $SOURCE_DIR/source/kudu/build.sh is_supported_platform; then
     $SOURCE_DIR/source/kudu/build.sh build
