db82756f73d6f96d19f1038c4557491456891adc
Daniel Becker <dbecker@apache.org>
2025-05-09 17:07:23 +0200

    IMPALA-14067: Bump glog version to 0.6.0 in toolchain
    This commit upgrades the glog library to version 0.6.0. This is a
    prerequisite of upgrading Impala's Kudu code to v1.17.1.
    
    Previously we had 3 patches on top of glog v0.3.5, but two of those are
    already present in v0.6.0 so we only need one patch.
    
    Testing:
     - Built native toolchain on local machine.
     - Built native toolchain with Jenkins job.
     - Built Impala with upgraded toolchain and ran exhaustive tests.
    
    Change-Id: Id2f353ffa8973b0c3d349e76201ffa2bf50f8871
    Reviewed-on: http://gerrit.cloudera.org:8080/22881
    Reviewed-by: Zoltan Borok-Nagy <boroknagyz@cloudera.com>
    Tested-by: Daniel Becker <daniel.becker@cloudera.com>

--- source/glog/build.sh
+++ source/glog/build.sh
@@ -30,10 +30,26 @@ if needs_build_package ; then
 
   setup_package_build $PACKAGE $PACKAGE_VERSION
 
-  GFLAGS_BUILD=$BUILD_DIR/gflags-$GFLAGS_VERSION
+  GFLAGS_CMAKE_CONF_DIR=$BUILD_DIR/gflags-$GFLAGS_VERSION/lib/cmake/gflags
 
-  wrap ./configure --with-gflags=$GFLAGS_BUILD --with-pic --prefix=$LOCAL_INSTALL $CONFIGURE_FLAG_BUILD_SYS
+  COMMON_GLOG_CMAKE_FLAGS="-Dgflags_DIR=$GFLAGS_CMAKE_CONF_DIR \
+    -DCMAKE_INSTALL_PREFIX=$LOCAL_INSTALL -DCMAKE_BUILD_TYPE=RELEASE -DWITH_TLS=OFF"
+
+  # glog's CMake builds either a shared or a static library but not both. Build each one
+  # separately.
+  rm -rf build_shared
+  mkdir build_shared
+  pushd build_shared
+  wrap cmake -DBUILD_SHARED_LIBS=ON $COMMON_GLOG_CMAKE_FLAGS ..
   wrap make VERBOSE=1 -j${BUILD_THREADS:-4} install
+  popd
+
+  rm -rf build_static
+  mkdir build_static
+  pushd build_static
+  wrap cmake -DBUILD_SHARED_LIBS=OFF $COMMON_GLOG_CMAKE_FLAGS ..
+  wrap make VERBOSE=1 -j${BUILD_THREADS:-4} install
+  popd
 
   finalize_package_build $PACKAGE $PACKAGE_VERSION
 fi
